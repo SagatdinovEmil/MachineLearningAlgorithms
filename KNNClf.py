76685d54c0964c8a7702a8c543b890c774ae638f
Sagatdinov Emil <sagatdinov2002@mail.ru>
2023-11-13 16:20:50 +0500

    add fit method

--- KNNClf.py
+++ KNNClf.py
@@ -6,9 +6,17 @@ from typing import Union, NoReturn, Dict, Callable
 class MyKNNClf:
     def __init__(self, k: int = 3):
         self.k = k
+        self.train_size: tuple
+        self.X: pd.DataFrame
+        self.y: pd.Series
 
     def __str__(self):
         return f"MyKNNClf class: k={self.k}"
 
     def __call__(self):
         return f"MyKNNClf class: k={self.k}"
+
+    def fit(self, X, y):
+        self.X = X
+        self.y = y
+        self.train_size = X.shape
