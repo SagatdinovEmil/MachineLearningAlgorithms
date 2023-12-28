0c55bdf03cd9910ba1fc891ac09609eaa073db20
Sagatdinov Emil <sagatdinov2002@mail.ru>
2023-12-28 19:43:04 +0500

    some fixes and added predict_proba method

--- KNNClf.py
+++ KNNClf.py
@@ -4,11 +4,12 @@ import random
 from typing import Union, NoReturn, Dict, Callable
 
 class MyKNNClf:
-    def __init__(self, k: int = 3):
+    def __init__(self, k: int = 3, metric: str = "euclidean"):
         self.k = k
         self.train_size: tuple
         self.X: pd.DataFrame
         self.y: pd.Series
+        self.metric = metric
 
     def __str__(self):
         return f"MyKNNClf class: k={self.k}"
@@ -16,28 +17,35 @@ class MyKNNClf:
     def __call__(self):
         return f"MyKNNClf class: k={self.k}"
 
-    def fit(self, X, y):
+    def fit(self, X: pd.DataFrame, y: pd.Series):
         self.X = X
         self.y = y
         self.train_size = X.shape
 
-    def predict(self, X):
-        result = np.zeros(len(X))
+    def predict(self, X: pd.DataFrame) -> np.array:
+        result = np.zeros(len(X), dtype=int)
         for j in range(len(X)):
-            distances = np.zeros(self.train_size)
+            distances = np.zeros(self.train_size[0])
             for i in range(len(self.X)):
-                distances[i] = self.calc_metric(X[j], self.X[i])
+                distances[i] = self.calc_metric(X.iloc[j], self.X.iloc[i])
             indices_of_nearest = np.argpartition(distances, self.k)[:self.k]
             fashion = 1 if np.mean(self.y[indices_of_nearest]) >= 0.5 else 0
             result[j] = fashion
         return result
 
 
-    def predict_proba(self, X):
-        pass
+    def predict_proba(self, X: pd.DataFrame) -> np.array:
+        result = np.zeros(len(X))
+        for j in range(len(X)):
+            distances = np.zeros(self.train_size[0])
+            for i in range(len(self.X)):
+                distances[i] = self.calc_metric(X.iloc[j], self.X.iloc[i])
+            indices_of_nearest = np.argpartition(distances, self.k)[:self.k]
+            result[j] = np.mean(self.y[indices_of_nearest])
+        return result
 
-    def calc_metric(self, x1, x2):
+    def calc_metric(self, x1: pd.Series, x2: pd.Series) -> float:
         sum = 0
         for i in range(len(x1)):
-            sum += (x1 - x2) ** 2
+            sum += (x1.iloc[i] - x2.iloc[i]) ** 2
         return sum ** 0.5
