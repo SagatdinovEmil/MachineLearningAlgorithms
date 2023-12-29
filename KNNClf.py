dd3b9a6f881648eb5f03a87afcd9346637f51c43
Sagatdinov Emil <sagatdinov2002@mail.ru>
2023-12-29 19:33:13 +0500

    Added weighted distances

--- KNNClf.py
+++ KNNClf.py
@@ -5,12 +5,13 @@ import random
 from typing import Union, NoReturn, Dict, Callable
 
 class MyKNNClf:
-    def __init__(self, k: int = 3, metric: str = "euclidean"):
+    def __init__(self, k: int = 3, metric: str = "euclidean", weight: str = "uniform"):
         self.k = k
         self.train_size: tuple
         self.X: pd.DataFrame
         self.y: pd.Series
         self.metric = metric
+        self.weight = weight
 
     def __str__(self):
         return f"MyKNNClf class: k={self.k}"
@@ -30,8 +31,38 @@ class MyKNNClf:
             for i in range(len(self.X)):
                 distances[i] = self.calc_metric(X.iloc[j], self.X.iloc[i])
             indices_of_nearest = np.argpartition(distances, self.k)[:self.k]
-            fashion = 1 if np.mean(self.y[indices_of_nearest]) >= 0.5 else 0
-            result[j] = fashion
+            indices_of_nearest = indices_of_nearest[np.argsort(distances[indices_of_nearest])]
+            distances = distances[indices_of_nearest]
+            nearest_y = self.y[indices_of_nearest]
+            if self.weight == "uniform":
+                mode = 1 if np.mean(nearest_y) >= 0.5 else 0
+                result[j] = mode
+            elif self.weight == "rank":
+                nominator0 = 0
+                nominator1 = 0
+                denominator = 0
+                for i in range(1, self.k + 1):
+                    denominator += 1 / i
+                    if nearest_y.iloc[i-1] == 1:
+                        nominator1 += 1 / i
+                    else:
+                        nominator0 += 1 / i
+                q0 = nominator0/denominator
+                q1 = nominator1/denominator
+                result[j] = 1 if q1 >= q0 else 0
+            elif self.weight == "distance":
+                nominator0 = 0
+                nominator1 = 0
+                denominator = 0
+                for i in range(1, self.k + 1):
+                    denominator += 1 / distances[i-1]
+                    if nearest_y.iloc[i-1] == 1:
+                        nominator1 += 1 / distances[i-1]
+                    else:
+                        nominator0 += 1 / distances[i-1]
+                q0 = nominator0/denominator
+                q1 = nominator1/denominator
+                result[j] = 1 if q1 >= q0 else 0
         return result
 
 
@@ -42,7 +73,29 @@ class MyKNNClf:
             for i in range(len(self.X)):
                 distances[i] = self.calc_metric(X.iloc[j], self.X.iloc[i])
             indices_of_nearest = np.argpartition(distances, self.k)[:self.k]
-            result[j] = np.mean(self.y[indices_of_nearest])
+            indices_of_nearest = indices_of_nearest[np.argsort(distances[indices_of_nearest])]
+            distances = distances[indices_of_nearest]
+            nearest_y = self.y[indices_of_nearest]
+            if self.weight == "uniform":
+                result[j] = np.mean(self.y[indices_of_nearest])
+            elif self.weight == "rank":
+                nominator1 = 0
+                denominator = 0
+                for i in range(1, self.k + 1):
+                    denominator += 1 / i
+                    if nearest_y.iloc[i-1] == 1:
+                        nominator1 += 1 / i
+                q1 = nominator1/denominator
+                result[j] = q1
+            elif self.weight == "distance":
+                nominator1 = 0
+                denominator = 0
+                for i in range(1, self.k + 1):
+                    denominator += 1 / distances[i-1]
+                    if nearest_y.iloc[i-1] == 1:
+                        nominator1 += 1 / distances[i-1]
+                q1 = nominator1/denominator
+                result[j] = q1
         return result
 
     def calc_metric(self, x1: pd.Series, x2: pd.Series) -> float:
